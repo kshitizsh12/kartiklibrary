d477e52813d0168fdf56e1c53206dafc165bd144
Sharmakartik <Kartik.gs@outlook.com>
2019-03-31 17:20:46 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,6 @@
 def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
+    bat 'mvn -version'
     echo "Hello, ${name}."
 }
