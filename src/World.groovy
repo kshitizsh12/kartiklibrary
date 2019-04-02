bb7e02cb6f59489d3e14a9ed6c63cd61f85a03e8
Sharmakartik <Kartik.gs@outlook.com>
2019-04-02 22:05:36 +0530

    Update World.groovy

--- src/World.groovy
+++ src/World.groovy
@@ -1,1 +1,9 @@
+@Grab(group = 'org.apache.commons', module = 'commons-lang3', version = '3.6')
+import org.apache.commons.lang3.StringUtils
 
+ class World implements Serializable{
+
+   def hello(){
+       println "my name is kartik sharma"
+      }
+}
