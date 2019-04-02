acac25365aba4acd1145db38440252ef8d65b6de
Sharmakartik <Kartik.gs@outlook.com>
2019-04-02 21:18:05 +0530

    Create World.groovy

---
+++ vars/World.groovy
@@ -0,0 +1,10 @@
+    
+@Grab(group = 'org.apache.commons', module = 'commons-lang3', version = '3.6')
+import org.apache.commons.lang3.StringUtils
+
+
+public class World{
+
+ static def hello(){
+ println "my name is kartik sharma"
+ }
