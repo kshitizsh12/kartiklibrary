3c93e1d7b0dacaabee2b6e1a9e4962c0d0c678f3
Sharmakartik <Kartik.gs@outlook.com>
2019-04-02 21:25:07 +0530

    Update World.groovy

--- vars/World.groovy
+++ vars/World.groovy
@@ -2,10 +2,9 @@
 @Grab(group = 'org.apache.commons', module = 'commons-lang3', version = '3.6')
 import org.apache.commons.lang3.StringUtils
 
+ class World{
 
-public class World{
-
- static def hello(){
- println "my name is kartik sharma"
- }
+   def hello(){
+       println "my name is kartik sharma"
+      }
 }
