48bf7f8e074345ad6f6cbe9986fa91a4c23c14ff
Sharmakartik <Kartik.gs@outlook.com>
2019-04-09 23:22:53 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,6 +2,10 @@
 def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
+    sh '''
+       maven -version
+       
+       '''
      echo "${env.BRANCH_NAME}"
      echo "${env.BUILD_NUMBER}"
     echo "Hello, ${name}."
