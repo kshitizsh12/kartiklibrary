d7cda23bebfa162bf3b1c43147c8673e90fc0c13
Sharmakartik <Kartik.gs@outlook.com>
2019-04-11 13:14:55 +0530

    Update World.groovy

--- src/World.groovy
+++ src/World.groovy
@@ -1,10 +1,9 @@
 import java.io.Serializable
 
 class World implements Serializable{
-    def steps
-    World(steps) {this.steps = steps}
    
-   def hello(){
-       println "my name is kartik sharma"
+   
+   def String hello(){
+       return "Loading from class level."
       }
 }
