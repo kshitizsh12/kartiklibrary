fe69f1b37ad1278015384746fb75757dfaad2442
Sharmakartik <Kartik.gs@outlook.com>
2019-04-11 19:47:39 +0530

    Update Utilities.groovy

--- src/Utilities.groovy
+++ src/Utilities.groovy
@@ -4,6 +4,6 @@ class Utilities implements Serializable {
   def steps
   Utilities(steps) {this.steps = steps}
   def mvn(args) {
-    steps.sh "${steps.tool 'jenkins-mvn'} -o ${args}"
+    steps.sh "${steps.tool 'jenkins-mvn'}/bin/mvn -o ${args}"
   }
 }
