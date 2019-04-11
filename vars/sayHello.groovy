927cbbb86b0bd48ef389fa31b97ee32ff827b943
Sharmakartik <Kartik.gs@outlook.com>
2019-04-11 12:19:20 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,9 +3,9 @@ import src.World
 def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-   
-     
      echo "${env.BRANCH_NAME}"
      echo "${env.BUILD_NUMBER}"
     echo "Hello, ${name}."
+    World w= new World()
+    w.hello()
 }
