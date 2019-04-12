71cd10a3b1c7c98c61a4cfc9246d58d33c71c59d
Sharmakartik <Kartik.gs@outlook.com>
2019-04-12 15:28:09 +0530

    Update emailnotify.groovy

--- vars/emailnotify.groovy
+++ vars/emailnotify.groovy
@@ -2,16 +2,26 @@
 
 def call(String buildResult){
  if ( buildResult == "SUCCESS" ) {
+  mail to: '',
+    subject: "Job '${JOB_NAME}' (${BUILD_NUMBER}) is waiting for input",
+    body: "Please go to ${BUILD_URL} and verify the build"
     
   }
   else if( buildResult == "FAILURE" ) { 
-    
+    mail to: '',
+    subject: "Job '${JOB_NAME}' (${BUILD_NUMBER}) is waiting for input",
+    body: "Please go to ${BUILD_URL} and verify the build"
   }
   else if( buildResult == "UNSTABLE" ) { 
+   mail to: 'devops@acme.com',
+    subject: "Job '${JOB_NAME}' (${BUILD_NUMBER}) is waiting for input",
+    body: "Please go to ${BUILD_URL} and verify the build"
     
   }
   else {
-    
+    mail to: '',
+    subject: "Job '${JOB_NAME}' (${BUILD_NUMBER}) is waiting for input",
+    body: "Please go to ${BUILD_URL} and verify the build"
   }
 
 }
