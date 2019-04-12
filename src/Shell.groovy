ba8de087db15b9c11137880fed0f6e04a9a8f6dd
Sharmakartik <Kartik.gs@outlook.com>
2019-04-12 13:27:36 +0530

    Update Shell.groovy

--- src/Shell.groovy
+++ src/Shell.groovy
@@ -3,11 +3,7 @@
 class Shell implements Serializable {
   def steps
   Shell(steps) {this.steps = steps}
-  def cmmddd() {
-    steps.sh "df -h"
-    steps.sh "mvn -version"
-    //steps.sh "mvn -B -DskipTests clean package"
-  }
+
     def mvn(args) {
     steps.sh "${steps.tool 'jenkins-mvn'}/bin/mvn -o ${args}"
   }
