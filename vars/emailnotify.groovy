0eb9ee558754d8ff551ebc445ed3681356cf699f
Sharmakartik <Kartik.gs@outlook.com>
2019-04-12 15:18:25 +0530

    Update emailnotify.groovy

--- vars/emailnotify.groovy
+++ vars/emailnotify.groovy
@@ -1,1 +1,17 @@
+#!/usr/bin/groovy
 
+def call(String buildResult){
+ if ( buildResult == "SUCCESS" ) {
+    
+  }
+  else if( buildResult == "FAILURE" ) { 
+    
+  }
+  else if( buildResult == "UNSTABLE" ) { 
+    
+  }
+  else {
+    
+  }
+
+}
